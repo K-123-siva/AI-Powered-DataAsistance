60fa1ac9f6921855d96a9b63e10ec59e4948d418
basavaadarsh <basavaadarsh4@gmail.com>
2024-08-08 16:37:35 +0530

    removed apikey

--- hugging_face.py
+++ hugging_face.py
@@ -8,7 +8,7 @@ import streamlit as st
 import pandas as pd
 
 # Define your Hugging Face API key here
-apikey = "HUGGINGFACE_TOKEN_REMOVED"
+apikey = ""
 
 # Login with your Hugging Face API key
 login(token=apikey)
